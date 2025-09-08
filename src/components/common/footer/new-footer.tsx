e2d14110bd770e16aedd2cf7b748a885cae90e86
Bhuvan Bhattarai <bhuvanbhattarai07@gmail.com>
2025-09-08 13:58:01 +0545

    upd fooot

--- src/components/common/footer/new-footer.tsx
+++ src/components/common/footer/new-footer.tsx
@@ -63,7 +63,7 @@ export default function Footer() {
               </div>
             </div>
           </div>
-          <div className="  border-t border-dashed pt-4 pb-2 text-sm mt-4 w-full flex flex-col justify-between items-center gap-2  flex-wrap mx-auto">
+          <div className="border-t border-dashed pt-4 pb-2 text-sm mt-4 w-full flex flex-col justify-between items-center gap-2  flex-wrap mx-auto">
 
             <div className="max-w-[1200px] w-full mx-auto">
               <div className="flex w-full justify-between items-center gap-5 md:gap-6 flex-wrap">
